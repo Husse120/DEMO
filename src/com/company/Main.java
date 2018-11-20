bfd3d81e5b784e9d692e0a76809ebbc9380ea6a2
Hussain Al Mashkor <axerlexoffcial@gmail.com>
2018-11-21 00:41:00 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("this is the testtesttest");
+        System.out.println("this is the test");
 
     }
 }
